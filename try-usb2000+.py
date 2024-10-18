a403bf30f07389325ee61d91fd29d85bba8d0f3f
David Fokkema <davidfokkema@icloud.com>
2024-10-18 21:07:16 +0200

    Continuously take spectra

--- try-usb2000+.py
+++ try-usb2000+.py
@@ -8,22 +8,28 @@ import usb.util
 dev = usb.core.find(idVendor=0x2457, idProduct=0x101E)
 dev.set_configuration()
 dev.write(0x01, b"\x01")
-time.sleep(.1)
-dev.write(0x01, b"\x09")
-packets = []
-while True:
-    try:
-        packets.append(dev.read(0x82, 512).tobytes())
-    except usb.core.USBTimeoutError:
-        break
-assert packets[-1][-1] == 0x69
+
+try:
+    while True:
+        dev.write(0x01, b"\x09")
+        packets = []
+        while True:
+            try:
+                packets.append(dev.read(0x82, 512).tobytes())
+            except usb.core.USBTimeoutError:
+                break
+        assert packets[-1][-1] == 0x69
+
+        pixels = array.array("H", b"".join(packets[:-1]))
+
+        print(f"Got {len(pixels)} pixels")
+except KeyboardInterrupt:
+    pass
+
 # Shutdown
 dev.write(0x01, b"\x04\x00\x00")
 
-pixels = array.array("H", b"".join(packets[:-1]))
-
-print(f"Got {len(pixels)} pixels")
-
+# plots
 plt.figure()
 plt.plot(pixels[20:])
 plt.show()
