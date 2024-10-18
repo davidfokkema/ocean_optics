27f699d6870ad994c21df1a948a4be2cc6f62d19
David Fokkema <davidfokkema@icloud.com>
2024-10-18 20:52:22 +0200

    Some error handling, shutdown and plotext

--- try-usb2000+.py
+++ try-usb2000+.py
@@ -1,4 +1,5 @@
 import array
+import time
 
 import matplotlib.pyplot as plt
 import usb.core
@@ -7,13 +8,26 @@ import usb.util
 dev = usb.core.find(idVendor=0x2457, idProduct=0x101E)
 dev.set_configuration()
 dev.write(0x01, b"\x01")
+time.sleep(.1)
 dev.write(0x01, b"\x09")
 packets = []
-for _ in range(8):
-    packets.append(dev.read(0x82, 512).tobytes())
-assert dev.read(0x82, 1).tobytes() == b"\x69"
+while True:
+    try:
+        packets.append(dev.read(0x82, 512).tobytes())
+    except usb.core.USBTimeoutError:
+        break
+assert packets[-1][-1] == 0x69
+# Shutdown
+dev.write(0x01, b"\x04\x00\x00")
 
-pixels = array.array("H", b"".join(packets))
+pixels = array.array("H", b"".join(packets[:-1]))
+
+print(f"Got {len(pixels)} pixels")
+
 plt.figure()
-plt.plot(pixels[2:])
+plt.plot(pixels[20:])
+plt.show()
+
+import plotext as plt
+plt.plot(pixels[20:])
 plt.show()
